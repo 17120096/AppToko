aeb1a82ed5d90d4f05161c5276f324e78809df13
dian_agus <dian.30@students.amikom.ac.id>
2023-01-21 17:12:22 +0700

    login dengan session

--- app/src/main/java/com/example/apptoko/LoginActivity.kt
+++ app/src/main/java/com/example/apptoko/LoginActivity.kt
@@ -1,5 +1,7 @@
 package com.example.apptoko
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,12 +9,20 @@ import android.widget.Button
 import android.widget.Toast
 import com.example.apptoko.api.BaseRetrofit
 import com.example.apptoko.response.login.LoginResponse
+import com.example.apptoko.utils.SessionManager
 import com.google.android.material.textfield.TextInputEditText
+import com.google.gson.Gson
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
+
+    companion object{
+        lateinit var sessionManager: SessionManager
+        private lateinit var context: Context
+    }
 
     private  val api by lazy { BaseRetrofit().endpoint }
 
@@ -20,12 +30,22 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        sessionManager = SessionManager(this)
+
+        val loginStatus = sessionManager.getBoolean("LOGIN_STATUS")
+        if (loginStatus){
+            val moveIntent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(moveIntent)
+            finish()
+        }
+
+
         val btnLogin = findViewById(R.id.btnLogin) as Button
         val txtEmail = findViewById(R.id.txtEmail) as TextInputEditText
         val txtPassword = findViewById(R.id.txtPassword) as TextInputEditText
 
         btnLogin.setOnClickListener{
-            Toast.makeText(this,"Login sedang diproses", Toast.LENGTH_SHORT).show()
+
 
             api.login(txtEmail.text.toString(), txtPassword.text.toString()).enqueue(object :
                 Callback<LoginResponse> {
@@ -34,6 +54,23 @@ class LoginActivity : AppCompatActivity() {
                     response: Response<LoginResponse>
                 ) {
                     Log.e("Login Data", response.toString())
+
+                    val correct = if (response.body() != null) response.body()!!.success else false
+
+                    if (correct){
+                        val token = response.body()!!.data.token
+
+                        sessionManager.saveString("TOKEN", "Bearer $token")
+                        sessionManager.saveBoolean("LOGIN_STATUS", true)
+
+                        val moveIntent = Intent(this@LoginActivity, MainActivity::class.java)
+                        startActivity(moveIntent)
+                        finish()
+
+                        Toast.makeText(applicationContext,"Login berhasil!", Toast.LENGTH_LONG).show()
+                    }else{
+                        Toast.makeText(applicationContext,"Email atau password Anda salah!", Toast.LENGTH_LONG).show()
+                    }
                 }
 
                 override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
